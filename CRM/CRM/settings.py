c66e04b3ccf99a9a4ac3331fbe6882bb207afceb
15755112589 <1187526816@qq.com>
2020-06-14 14:14:53 +0800

    modify mysql password

--- CRM/CRM/settings.py
+++ CRM/CRM/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'crm',
         # 'HOST': '127.0.0.1',
-        'HOST': '59.110.152.148',
+        'HOST': '127.0.0.1',
         'PORT': 3306,
         'USER': 'root',
         'PASSWORD': 'wuyujuan662'
